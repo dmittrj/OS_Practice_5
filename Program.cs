9e820d5b6ccc8239451941ab64145e01d479d3a0
Дмитрий Балабанов <dmittrj@outlook.com>
2022-03-31 19:00:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OS_Practice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
