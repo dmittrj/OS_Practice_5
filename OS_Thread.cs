40d75e5da0a99ea0293044b5b0dd28ddb828bf54
Дмитрий Балабанов <dmittrj@outlook.com>
2022-04-02 12:50:37 +0300

    Table with threads

---
+++ OS_Thread.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Practice_5
+{
+    enum OS_ThreadStatus
+    {
+        None, Created, Ready, Running, Awaiting, Executed
+    }
+    class OS_Thread
+    {
+        public OS_ThreadStatus T_Status;
+        public bool T_isSelected;
+
+        public OS_Thread()
+        {
+            T_Status = OS_ThreadStatus.None;
+            T_isSelected = false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,63 @@ namespace OS_Practice_5
 {
     class Program
     {
+        public static int OS_CurrentCursorPosition = 0;
+        public static OS_Thread[] OS_Threads = new OS_Thread[3];
+        public static void OS_DrawTableLine(int position)
+        {
+            if (OS_CurrentCursorPosition == position)
+            {
+                Console.Write(" >");
+            } 
+            else
+            {
+                Console.Write("  ");
+            }
+            if (OS_Threads[position].T_isSelected)
+            {
+                Console.Write(" [*]");
+            }
+            else
+            {
+                Console.Write(" [ ]");
+            }
+            switch (OS_Threads[position].T_Status)
+            {
+                case OS_ThreadStatus.None:
+                    Console.Write(" Не запущен");
+                    break;
+                case OS_ThreadStatus.Created:
+                    break;
+                case OS_ThreadStatus.Ready:
+                    break;
+                case OS_ThreadStatus.Running:
+                    break;
+                case OS_ThreadStatus.Awaiting:
+                    break;
+                case OS_ThreadStatus.Executed:
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine();
+        }
+        public static void OS_PrintTable()
+        {
+            Console.Clear();
+            Console.Write("\n Потоки:\n");
+            for (int i = 0; i < 3; i++)
+            {
+                OS_DrawTableLine(i);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < OS_Threads.Length; i++)
+            {
+                OS_Threads[i] = new OS_Thread();
+            }
+            OS_PrintTable();
         }
     }
 }
