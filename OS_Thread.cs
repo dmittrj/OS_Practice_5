137076243fad8578d45c0112d0cce8382733ec24
Дмитрий Балабанов <dmittrj@outlook.com>
2022-04-03 12:21:09 +0300

    Two threads

--- OS_Thread.cs
+++ OS_Thread.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OS_Practice_5
@@ -10,15 +11,92 @@ namespace OS_Practice_5
     {
         None, Created, Ready, Running, Awaiting, Executed
     }
+    enum OS_Task
+    {
+        Primes, Summation, Factorials
+    }
     class OS_Thread
     {
-        public OS_ThreadStatus T_Status;
-        public bool T_isSelected;
+        private const int MAX_NUMBER = 400000;
 
-        public OS_Thread()
+        public OS_ThreadStatus T_Status;
+        public OS_Task T_Task;
+        public bool T_isSelected;
+        public int T_Priority;
+        public Thread T_Thread;
+
+        public OS_Thread(OS_Task task)
         {
             T_Status = OS_ThreadStatus.None;
             T_isSelected = false;
+            T_Priority = 1;
+            T_Task = task;
+            T_Thread = new Thread(OS_PerformTask);
+        }
+
+        public void OS_Start()
+        {
+            T_Thread.Start();
+        }
+
+        private void OS_PerformTask()
+        {
+            switch (T_Task)
+            {
+                case OS_Task.Primes:
+                    OS_PerformTask_Primes();
+                    break;
+                case OS_Task.Summation:
+                    OS_PerformTask_Summation();
+                    break;
+                case OS_Task.Factorials:
+                    OS_PerformTask_Factorials();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OS_PerformTask_Primes()
+        {
+            List<int> primes = new List<int>();
+            for (int i = 0; i < MAX_NUMBER; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+            }
+            foreach (int item in primes)
+            {
+                Console.Write(item.ToString() + ", ");
+            }
+        }
+
+        private void OS_PerformTask_Summation()
+        {
+            ulong sum = 0;
+            for (int i = 0; i < MAX_NUMBER; i++)
+            {
+                if (i % 2 == 1 && i % 10 != 0)
+                    sum += (ulong)i * (ulong)i;
+                else
+                    sum += (ulong)i;
+            }
+            Console.WriteLine(sum);
+        }
+
+        private void OS_PerformTask_Factorials()
+        {
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace OS_Practice_5
     {
         public static int OS_CurrentCursorPosition = 0;
         public static OS_Thread[] OS_Threads = new OS_Thread[3];
+        public static int OS_Wheel = 0;
         public static void OS_DrawTableLine(int position)
         {
             if (OS_CurrentCursorPosition == position)
@@ -27,13 +28,93 @@ namespace OS_Practice_5
             switch (OS_Threads[position].T_Status)
             {
                 case OS_ThreadStatus.None:
-                    Console.Write(" Не запущен");
+                    Console.Write(" Не создан");
                     break;
                 case OS_ThreadStatus.Created:
+                    switch (OS_Threads[position].T_Task)
+                    {
+                        case OS_Task.Primes:
+                            Console.Write(" Поток 1: поиск простых чисел    ");
+                            break;
+                        case OS_Task.Summation:
+                            Console.Write(" Поток 2: суммирование чисел     ");
+                            break;
+                        case OS_Task.Factorials:
+                            Console.Write(" Поток 3: нахождение факториалов ");
+                            break;
+                        default:
+                            break;
+                    }
+                    for (int i = 0; i < OS_Threads[position].T_Priority; i++)
+                    {
+                        Console.Write("/");
+                    }
+                    for (int i = 0; i < 5 - OS_Threads[position].T_Priority; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("  Создан ");
+                    switch (OS_Wheel)
+                    {
+                        case 0:
+                            Console.Write("/");
+                            break;
+                        case 1:
+                            Console.Write("-");
+                            break;
+                        case 2:
+                            Console.Write("\\");
+                            break;
+                        case 3:
+                            Console.Write("|");
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case OS_ThreadStatus.Ready:
                     break;
                 case OS_ThreadStatus.Running:
+                    switch (OS_Threads[position].T_Task)
+                    {
+                        case OS_Task.Primes:
+                            Console.Write(" Поток 1: поиск простых чисел    ");
+                            break;
+                        case OS_Task.Summation:
+                            Console.Write(" Поток 2: суммирование чисел     ");
+                            break;
+                        case OS_Task.Factorials:
+                            Console.Write(" Поток 3: нахождение факториалов ");
+                            break;
+                        default:
+                            break;
+                    }
+                    for (int i = 0; i < OS_Threads[position].T_Priority; i++)
+                    {
+                        Console.Write("/");
+                    }
+                    for (int i = 0; i < 5 - OS_Threads[position].T_Priority; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("  Запущен ");
+                    switch (OS_Wheel)
+                    {
+                        case 0:
+                            Console.Write("/");
+                            break;
+                        case 1:
+                            Console.Write("-");
+                            break;
+                        case 2:
+                            Console.Write("\\");
+                            break;
+                        case 3:
+                            Console.Write("|");
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case OS_ThreadStatus.Awaiting:
                     break;
@@ -52,15 +133,361 @@ namespace OS_Practice_5
             {
                 OS_DrawTableLine(i);
             }
+            OS_Wheel++;
+            OS_Wheel %= 4;
+            Console.WriteLine("\n __________________________\n");
+            switch (OS_Threads[OS_CurrentCursorPosition].T_Status)
+            {
+                case OS_ThreadStatus.None:
+                    Console.WriteLine(" Space - выбрать");
+                    Console.Write(" C - создать");
+                    break;
+                case OS_ThreadStatus.Created:
+                    Console.WriteLine(" Space - выбрать");
+                    Console.Write(" Enter - запустить   +/- - изменить приоритет");
+                    break;
+                case OS_ThreadStatus.Ready:
+                    break;
+                case OS_ThreadStatus.Running:
+                    break;
+                case OS_ThreadStatus.Awaiting:
+                    break;
+                case OS_ThreadStatus.Executed:
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine();
+            ConsoleKey key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.Backspace:
+                    break;
+                case ConsoleKey.Tab:
+                    break;
+                case ConsoleKey.Clear:
+                    break;
+                case ConsoleKey.Enter:
+                    if (OS_Threads[OS_CurrentCursorPosition].T_Status != OS_ThreadStatus.None)
+                    {
+                        OS_Threads[OS_CurrentCursorPosition].T_Status = OS_ThreadStatus.Running;
+                        OS_Threads[OS_CurrentCursorPosition].OS_Start();
+                    }
+                    break;
+                case ConsoleKey.Pause:
+                    break;
+                case ConsoleKey.Escape:
+                    break;
+                case ConsoleKey.Spacebar:
+                    OS_Threads[OS_CurrentCursorPosition].T_isSelected = !OS_Threads[OS_CurrentCursorPosition].T_isSelected;
+                    break;
+                case ConsoleKey.PageUp:
+                    break;
+                case ConsoleKey.PageDown:
+                    break;
+                case ConsoleKey.End:
+                    break;
+                case ConsoleKey.Home:
+                    break;
+                case ConsoleKey.LeftArrow:
+                    break;
+                case ConsoleKey.UpArrow:
+                    OS_CurrentCursorPosition--;
+                    if (OS_CurrentCursorPosition < 0)
+                        OS_CurrentCursorPosition = 0;
+                    break;
+                case ConsoleKey.RightArrow:
+                    break;
+                case ConsoleKey.DownArrow:
+                    OS_CurrentCursorPosition++;
+                    OS_CurrentCursorPosition %= 3;
+                    break;
+                case ConsoleKey.Select:
+                    break;
+                case ConsoleKey.Print:
+                    break;
+                case ConsoleKey.Execute:
+                    break;
+                case ConsoleKey.PrintScreen:
+                    break;
+                case ConsoleKey.Insert:
+                    break;
+                case ConsoleKey.Delete:
+                    break;
+                case ConsoleKey.Help:
+                    break;
+                case ConsoleKey.D0:
+                    break;
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    break;
+                case ConsoleKey.D3:
+                    break;
+                case ConsoleKey.D4:
+                    break;
+                case ConsoleKey.D5:
+                    break;
+                case ConsoleKey.D6:
+                    break;
+                case ConsoleKey.D7:
+                    break;
+                case ConsoleKey.D8:
+                    break;
+                case ConsoleKey.D9:
+                    break;
+                case ConsoleKey.A:
+                    break;
+                case ConsoleKey.B:
+                    break;
+                case ConsoleKey.C:
+                    if (OS_Threads[OS_CurrentCursorPosition].T_Status == OS_ThreadStatus.None)
+                    {
+                        OS_Threads[OS_CurrentCursorPosition].T_Status = OS_ThreadStatus.Created;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.F:
+                    break;
+                case ConsoleKey.G:
+                    break;
+                case ConsoleKey.H:
+                    break;
+                case ConsoleKey.I:
+                    break;
+                case ConsoleKey.J:
+                    break;
+                case ConsoleKey.K:
+                    break;
+                case ConsoleKey.L:
+                    break;
+                case ConsoleKey.M:
+                    break;
+                case ConsoleKey.N:
+                    break;
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.P:
+                    break;
+                case ConsoleKey.Q:
+                    break;
+                case ConsoleKey.R:
+                    break;
+                case ConsoleKey.S:
+                    break;
+                case ConsoleKey.T:
+                    break;
+                case ConsoleKey.U:
+                    break;
+                case ConsoleKey.V:
+                    break;
+                case ConsoleKey.W:
+                    break;
+                case ConsoleKey.X:
+                    break;
+                case ConsoleKey.Y:
+                    break;
+                case ConsoleKey.Z:
+                    break;
+                case ConsoleKey.LeftWindows:
+                    break;
+                case ConsoleKey.RightWindows:
+                    break;
+                case ConsoleKey.Applications:
+                    break;
+                case ConsoleKey.Sleep:
+                    break;
+                case ConsoleKey.NumPad0:
+                    break;
+                case ConsoleKey.NumPad1:
+                    break;
+                case ConsoleKey.NumPad2:
+                    break;
+                case ConsoleKey.NumPad3:
+                    break;
+                case ConsoleKey.NumPad4:
+                    break;
+                case ConsoleKey.NumPad5:
+                    break;
+                case ConsoleKey.NumPad6:
+                    break;
+                case ConsoleKey.NumPad7:
+                    break;
+                case ConsoleKey.NumPad8:
+                    break;
+                case ConsoleKey.NumPad9:
+                    break;
+                case ConsoleKey.Multiply:
+                    break;
+                case ConsoleKey.Add:
+                    break;
+                case ConsoleKey.Separator:
+                    break;
+                case ConsoleKey.Subtract:
+                    break;
+                case ConsoleKey.Decimal:
+                    break;
+                case ConsoleKey.Divide:
+                    break;
+                case ConsoleKey.F1:
+                    break;
+                case ConsoleKey.F2:
+                    break;
+                case ConsoleKey.F3:
+                    break;
+                case ConsoleKey.F4:
+                    break;
+                case ConsoleKey.F5:
+                    break;
+                case ConsoleKey.F6:
+                    break;
+                case ConsoleKey.F7:
+                    break;
+                case ConsoleKey.F8:
+                    break;
+                case ConsoleKey.F9:
+                    break;
+                case ConsoleKey.F10:
+                    break;
+                case ConsoleKey.F11:
+                    break;
+                case ConsoleKey.F12:
+                    break;
+                case ConsoleKey.F13:
+                    break;
+                case ConsoleKey.F14:
+                    break;
+                case ConsoleKey.F15:
+                    break;
+                case ConsoleKey.F16:
+                    break;
+                case ConsoleKey.F17:
+                    break;
+                case ConsoleKey.F18:
+                    break;
+                case ConsoleKey.F19:
+                    break;
+                case ConsoleKey.F20:
+                    break;
+                case ConsoleKey.F21:
+                    break;
+                case ConsoleKey.F22:
+                    break;
+                case ConsoleKey.F23:
+                    break;
+                case ConsoleKey.F24:
+                    break;
+                case ConsoleKey.BrowserBack:
+                    break;
+                case ConsoleKey.BrowserForward:
+                    break;
+                case ConsoleKey.BrowserRefresh:
+                    break;
+                case ConsoleKey.BrowserStop:
+                    break;
+                case ConsoleKey.BrowserSearch:
+                    break;
+                case ConsoleKey.BrowserFavorites:
+                    break;
+                case ConsoleKey.BrowserHome:
+                    break;
+                case ConsoleKey.VolumeMute:
+                    break;
+                case ConsoleKey.VolumeDown:
+                    break;
+                case ConsoleKey.VolumeUp:
+                    break;
+                case ConsoleKey.MediaNext:
+                    break;
+                case ConsoleKey.MediaPrevious:
+                    break;
+                case ConsoleKey.MediaStop:
+                    break;
+                case ConsoleKey.MediaPlay:
+                    break;
+                case ConsoleKey.LaunchMail:
+                    break;
+                case ConsoleKey.LaunchMediaSelect:
+                    break;
+                case ConsoleKey.LaunchApp1:
+                    break;
+                case ConsoleKey.LaunchApp2:
+                    break;
+                case ConsoleKey.Oem1:
+                    break;
+                case ConsoleKey.OemPlus:
+                    if (OS_Threads[OS_CurrentCursorPosition].T_Status != OS_ThreadStatus.None)
+                    {
+                        OS_Threads[OS_CurrentCursorPosition].T_Priority++;
+                        if (OS_Threads[OS_CurrentCursorPosition].T_Priority > 5)
+                            OS_Threads[OS_CurrentCursorPosition].T_Priority = 5;
+                    }
+                    break;
+                case ConsoleKey.OemComma:
+                    break;
+                case ConsoleKey.OemMinus:
+                    if (OS_Threads[OS_CurrentCursorPosition].T_Status != OS_ThreadStatus.None)
+                    {
+                        OS_Threads[OS_CurrentCursorPosition].T_Priority--;
+                        if (OS_Threads[OS_CurrentCursorPosition].T_Priority < 1)
+                            OS_Threads[OS_CurrentCursorPosition].T_Priority = 1;
+                    }
+                    break;
+                case ConsoleKey.OemPeriod:
+                    break;
+                case ConsoleKey.Oem2:
+                    break;
+                case ConsoleKey.Oem3:
+                    break;
+                case ConsoleKey.Oem4:
+                    break;
+                case ConsoleKey.Oem5:
+                    break;
+                case ConsoleKey.Oem6:
+                    break;
+                case ConsoleKey.Oem7:
+                    break;
+                case ConsoleKey.Oem8:
+                    break;
+                case ConsoleKey.Oem102:
+                    break;
+                case ConsoleKey.Process:
+                    break;
+                case ConsoleKey.Packet:
+                    break;
+                case ConsoleKey.Attention:
+                    break;
+                case ConsoleKey.CrSel:
+                    break;
+                case ConsoleKey.ExSel:
+                    break;
+                case ConsoleKey.EraseEndOfFile:
+                    break;
+                case ConsoleKey.Play:
+                    break;
+                case ConsoleKey.Zoom:
+                    break;
+                case ConsoleKey.NoName:
+                    break;
+                case ConsoleKey.Pa1:
+                    break;
+                case ConsoleKey.OemClear:
+                    break;
+                default:
+                    break;
+            }
         }
 
         static void Main(string[] args)
         {
             for (int i = 0; i < OS_Threads.Length; i++)
             {
-                OS_Threads[i] = new OS_Thread();
+                OS_Threads[i] = new OS_Thread((OS_Task)i);
             }
-            OS_PrintTable();
+            while (true) OS_PrintTable();
         }
     }
 }
